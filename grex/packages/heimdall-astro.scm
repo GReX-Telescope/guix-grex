64c9743386e641d263fbc3310d12a837c1d3a562
Kiran Shila <me@kiranshila.com>
2022-05-23 13:05:01 -0700

    Heimdall builds!!

--- grex/packages/heimdall-astro.scm
+++ grex/packages/heimdall-astro.scm
@@ -1,6 +1,7 @@
 (define-module (grex packages heimdall-astro)
   #:use-module (grex packages psrdada)
   #:use-module (grex packages dedisp)
+  #:use-module (grex packages cuda)
   #:use-module (guix packages)
   #:use-module (guix gexp)
   #:use-module (gnu packages)
@@ -8,11 +9,10 @@
   #:use-module (gnu packages boost)
   #:use-module (guix git-download)
   #:use-module (guix build-system cmake)
-  #:use-module (guix licenses)
-  #:use-module (grex packages cuda))
+  #:use-module (guix licenses))
 
 (define-public heimdall-astro
-  (let ((commit "05de726d2b78c41de6439eb12583da2437829e7d")
+  (let ((commit "d6047187e407cf22e6bd9a8bdfd77a683f6a1835")
         (revision "1"))
     (package
      (name "heimdall-astro")
@@ -22,10 +22,12 @@
               (uri (git-reference
                     (url "https://github.com/GReX-Telescope/heimdall-astro")
                     (commit commit)))
-              (sha256 (base32 "0yj0bizdzch37qksga1cw2l251il37vl0759z2swi5af13alnbj8"))))
+              (sha256
+               (base32 "0pc8xgjvl860ska5xgkmnk2j5fz2ljx73jq54x9wl74sd9cqvh7w"))))
      (build-system cmake-build-system)
+     (arguments '(#:tests? #f))
      (inputs (list cuda-11.7 psrdada dedisp boost))
      (synopsis "Transient Detection Pipeline")
      (description "")
-     (home-page "https://github.com/GReX-Telescope/heimdall")
+     (home-page "https://github.com/GReX-Telescope/heimdall-astro")
      (license gpl3))))
