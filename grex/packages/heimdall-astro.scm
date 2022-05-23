8597f179b54314c3e058cf2576fcd03e430f6b29
Kiran Shila <me@kiranshila.com>
2022-05-23 11:14:58 -0700

    No more dependency on guix-hpc-nonfree

--- grex/packages/heimdall-astro.scm
+++ grex/packages/heimdall-astro.scm
@@ -9,25 +9,23 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system cmake)
   #:use-module (guix licenses)
-  #:use-module (non-free cuda))
+  #:use-module (grex packages cuda))
 
 (define-public heimdall-astro
   (let ((commit "05de726d2b78c41de6439eb12583da2437829e7d")
         (revision "1"))
-    (package-with-c-toolchain
-     (package
-      (name "heimdall-astro")
-      (version (git-version "0.1" revision commit))
-      (source (origin
-               (method git-fetch)
-               (uri (git-reference
-                     (url "https://github.com/GReX-Telescope/heimdall-astro")
-                     (commit commit)))
-               (sha256 (base32 "0yj0bizdzch37qksga1cw2l251il37vl0759z2swi5af13alnbj8"))))
-      (build-system cmake-build-system)
-      (inputs (list cuda-11.0 psrdada dedisp boost))
-      (synopsis "Transient Detection Pipeline")
-      (description "")
-      (home-page "https://github.com/GReX-Telescope/heimdall")
-      (license gpl3))
-     `(("toolchain" ,(specification->package "gcc-toolchain@8"))))))
+    (package
+     (name "heimdall-astro")
+     (version (git-version "0.1" revision commit))
+     (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/GReX-Telescope/heimdall-astro")
+                    (commit commit)))
+              (sha256 (base32 "0yj0bizdzch37qksga1cw2l251il37vl0759z2swi5af13alnbj8"))))
+     (build-system cmake-build-system)
+     (inputs (list cuda-11.7 psrdada dedisp boost))
+     (synopsis "Transient Detection Pipeline")
+     (description "")
+     (home-page "https://github.com/GReX-Telescope/heimdall")
+     (license gpl3))))
