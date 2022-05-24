7eda4b61fe786223cc57786605452353eb47ad4c
Kiran Shila <me@kiranshila.com>
2022-05-24 10:04:00 -0700

    Bump revision

--- grex/packages/psrdada.scm
+++ grex/packages/psrdada.scm
@@ -11,7 +11,7 @@
 
 (define-public psrdada
   (let ((commit "78b01e857a765caade0c1efaaab4558913e035ec")
-        (revision "2"))
+        (revision "3"))
     (package
      (name "psrdada")
      (version (git-version "1.0.0" revision commit))
