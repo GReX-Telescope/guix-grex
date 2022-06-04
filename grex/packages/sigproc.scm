f813c71e5e88e541ec4b9eb81e7928a3ea17b241
Kiran Shila <me@kiranshila.com>
2022-06-03 17:00:01 -0700

    Add lxml

--- grex/packages/sigproc.scm
+++ grex/packages/sigproc.scm
@@ -8,6 +8,7 @@
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages astronomy)
   #:use-module (gnu packages commencement)
+  #:use-module (gnu packages xml)
   #:use-module (guix git-download)
   #:use-module (guix build-system gnu)
   #:use-module (guix licenses))
@@ -26,8 +27,7 @@
               (sha256
                (base32 "15qnjsmd84dwhfbqdk9khwms2558sn2h1qydlqwvn7w93bkbsny3"))))
      (build-system gnu-build-system)
-     (native-inputs (list libtool autoconf automake tcsh gfortran-toolchain))
-     (inputs (list fftw cfitsio))
+     (native-inputs (list libtool autoconf automake tcsh gfortran-toolchain fftw cfitsio libxml2))
      (synopsis "Evan Keane's fork of Michael Keith's release of Duncan Lorimer's SIGPROC")
      (description "SIGPROC is a package designed to standardize the initial analysis of
 the many types of fast-sampled pulsar data. Currently recognized machines are the
