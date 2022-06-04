94de8729b150712750795349793e0310b2c5365f
Kiran Shila <me@kiranshila.com>
2022-06-03 17:04:31 -0700

    Add perl

--- grex/packages/sigproc.scm
+++ grex/packages/sigproc.scm
@@ -4,6 +4,7 @@
   #:use-module (gnu packages base)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages algebra)
+  #:use-module (gnu packages perl)
   #:use-module (gnu packages shells)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages astronomy)
@@ -27,7 +28,8 @@
               (sha256
                (base32 "15qnjsmd84dwhfbqdk9khwms2558sn2h1qydlqwvn7w93bkbsny3"))))
      (build-system gnu-build-system)
-     (native-inputs (list libtool autoconf automake tcsh gfortran-toolchain fftw cfitsio libxml2))
+     (native-inputs (list
+                     libtool autoconf automake tcsh gfortran-toolchain fftw cfitsio libxml2 perl))
      (synopsis "Evan Keane's fork of Michael Keith's release of Duncan Lorimer's SIGPROC")
      (description "SIGPROC is a package designed to standardize the initial analysis of
 the many types of fast-sampled pulsar data. Currently recognized machines are the
