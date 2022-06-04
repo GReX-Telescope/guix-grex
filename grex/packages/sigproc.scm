102d2ebdc8b114aa651fff0aba9c8726f2727e07
Kiran Shila <me@kiranshila.com>
2022-06-03 17:17:22 -0700

    Switch to BL's sigproc
    maybe someone knows how to use automake

--- grex/packages/sigproc.scm
+++ grex/packages/sigproc.scm
@@ -6,6 +6,7 @@
   #:use-module (gnu packages algebra)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages shells)
+  #:use-module (gnu packages maths)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages astronomy)
   #:use-module (gnu packages commencement)
@@ -15,22 +16,22 @@
   #:use-module (guix licenses))
 
 (define-public sigproc
-  (let ((commit "8f3a9a6632f15a146dd9e058a9a1ec807d1f992d")
-        (revision "218"))
+  (let ((commit "50619d280329318a24c65902de43e2eb3368d2c6")
+        (revision "176"))
     (package
      (name "sigproc")
-     (version (git-version "2014.5" revision commit))
+     (version (git-version "5.0.4" revision commit))
      (source (origin
               (method git-fetch)
               (uri (git-reference
-                    (url "https://github.com/FRBs/sigproc")
+                    (url "https://github.com/UCBerkeleySETI/bl_sigproc")
                     (commit commit)))
               (sha256
                (base32 "15qnjsmd84dwhfbqdk9khwms2558sn2h1qydlqwvn7w93bkbsny3"))))
      (build-system gnu-build-system)
      (native-inputs (list
-                     libtool autoconf automake tcsh gfortran-toolchain fftw cfitsio libxml2 perl))
-     (synopsis "Evan Keane's fork of Michael Keith's release of Duncan Lorimer's SIGPROC")
+                     libtool autoconf automake tcsh gfortran-toolchain fftw cfitsio libxml2 perl gsl))
+     (synopsis "Michael Keith's release of Duncan Lorimer's SIGPROC")
      (description "SIGPROC is a package designed to standardize the initial analysis of
 the many types of fast-sampled pulsar data. Currently recognized machines are the
 Wide Band Arecibo Pulsar Processor (WAPP), the Penn State Pulsar Machine (PSPM),
