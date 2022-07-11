9c5d69b7910b187ffc476e111b47abfe6ae72409
Kiran Shila <me@kiranshila.com>
2022-07-11 15:41:45 -0700

    Try a different port

--- grex/system/base.scm
+++ grex/system/base.scm
@@ -125,7 +125,7 @@
      ;; Use the Prometheus Node Exporter Service to get data to Grafana
      (service prometheus-node-exporter-service-type
               (prometheus-node-exporter-configuration
-               (web-listen-address "0.0.0.0:9100")))
+               (web-listen-address "0.0.0.0:8082")))
 
      (modify-services
       %base-services
