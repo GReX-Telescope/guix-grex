502bd84bf448355d948727dcfb9a31d78938aecc
Kiran Shila <me@kiranshila.com>
2023-01-31 18:30:09 -0800

    Add missing import

--- grex/system/grex-01.scm
+++ grex/system/grex-01.scm
@@ -3,6 +3,7 @@
   #:declarative? #f
   #:use-module (gnu packages shells)
   #:use-module (gnu packages linux)
+  #:use-module (gnu packages networking)
   #:use-module (guix gexp)
   #:use-module (grex system base)
   #:use-module (gnu services networking)
