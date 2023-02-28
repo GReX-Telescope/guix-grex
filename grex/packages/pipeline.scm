2d532d7ffaeae564c5b56dee8d0607aab38c4904
Kiran Shila <me@kiranshila.com>
2023-02-27 19:23:09 -0800

    Bump snapctl

--- grex/packages/pipeline.scm
+++ grex/packages/pipeline.scm
@@ -66,8 +66,8 @@ The modular design of PSRDADA includes:
      (home-page "https://github.com/GReX-Telescope/psrdada"))))
 
 (define-public snapctl
-  (let ((commit "48aa9f341278f3d6eece35374279e645c8e3e7f3")
-        (revision "40"))
+  (let ((commit "0f6131834a7f721fcc8e5113fdcd7000fbb2bc84")
+        (revision "41"))
     (package
      (name "snapctl")
      (version (git-version "0.1.0" revision commit))
@@ -78,7 +78,7 @@ The modular design of PSRDADA includes:
                     (commit commit)))
               (sha256
                (base32
-                "1l9zvzr5k3qzdrw7fn202f4sjggkpmv6zbgdb17f48xf2j79xjvm"))))
+                "0cv37lh6g7jpv13lmsrlszrcndkx8d9k9x3m92y037wvbiw5d6zz"))))
      (build-system python-build-system)
      (propagated-inputs (list python-loguru python-casperfpga))
      (native-inputs (list python-black))
